1a571dadcb1620a92043d6f25d60beffe7c2e88c
serswik <megasergik@icloud.com>
2025-05-28 13:21:07 +0300

    Added cart class

---
+++ Cart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartSystem
+{
+    public class Cart
+    {
+        private List<Product> products;
+
+        public Cart()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public decimal GetTotalAmount()
+        {
+            return products.Sum(p => p.Price);
+        }
+
+
+    }
+}
